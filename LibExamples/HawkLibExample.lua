1306b7f8571a7e61f5f187a5091f1395af9ba942
Hanki <108697650+TheHanki@users.noreply.github.com>
2024-07-14 04:36:15 +0300

    Update and rename NewLibExample.lua to HawkLibExample.lua

--- LibExamples/HawkLibExample.lua
+++ LibExamples/HawkLibExample.lua
@@ -2,7 +2,7 @@
 _Hawk = "ohhahtuhthttouttpwuttuaunbotwo"
 
 --loadstring
-local Hawk = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHzCihan/IDream/main/Libs/NewLib.lua", true))()
+local Hawk = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheHanki/HawkHUB/main/LibSources/HawkLib.lua", true))()
 
 --------------------------------------------------------------------------
 
